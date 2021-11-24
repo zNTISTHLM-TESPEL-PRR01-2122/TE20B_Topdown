aa4df2b827ea56b834a53547ed0094d6eb773da3
Mikael Bergström <krank23@gmail.com>
2021-11-24 11:08:10 +0100

    Initial commit

---
+++ TE20B_Topdown/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Raylib_cs;
+
+Raylib.InitWindow(800, 600, "2D-topdown game");
+Raylib.SetTargetFPS(60);
+
+Texture2D playerImage = Raylib.LoadTexture("happy.png");
+Rectangle playerRect = new Rectangle(10, 10, 32, 32);
+
+while (!Raylib.WindowShouldClose())
+{
+  Raylib.BeginDrawing();
+
+  // Raylib.DrawRectangle(10, 10, 32, 32, Color.RAYWHITE);
+
+  Raylib.ClearBackground(Color.BLUE);
+  Raylib.DrawRectangleRec(playerRect, Color.LIME);
+  
+  Raylib.DrawTexture(playerImage, 45, 70, Color.WHITE);
+
+  Raylib.EndDrawing();
+}
