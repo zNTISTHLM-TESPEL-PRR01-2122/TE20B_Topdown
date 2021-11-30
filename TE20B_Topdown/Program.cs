dc96e35c20f3c9055947e8ab57e6e637d8de1916
Mikael Bergström <krank23@gmail.com>
2021-11-30 11:03:07 +0100

    Förflyttning!

--- TE20B_Topdown/Program.cs
+++ TE20B_Topdown/Program.cs
@@ -4,19 +4,25 @@ using Raylib_cs;
 Raylib.InitWindow(800, 600, "2D-topdown game");
 Raylib.SetTargetFPS(60);
 
+float speed = 2.3f;
+
 Texture2D playerImage = Raylib.LoadTexture("happy.png");
-Rectangle playerRect = new Rectangle(10, 10, 32, 32);
+Rectangle playerRect = new Rectangle(100, 70, playerImage.width, playerImage.height);
 
 while (!Raylib.WindowShouldClose())
 {
+  if (Raylib.IsKeyDown(KeyboardKey.KEY_W)) playerRect.y -= speed;
+  if (Raylib.IsKeyDown(KeyboardKey.KEY_S)) playerRect.y += speed;
+  if (Raylib.IsKeyDown(KeyboardKey.KEY_A)) playerRect.x -= speed;
+  if (Raylib.IsKeyDown(KeyboardKey.KEY_D)) playerRect.x += speed;
+  
   Raylib.BeginDrawing();
 
-  // Raylib.DrawRectangle(10, 10, 32, 32, Color.RAYWHITE);
-
   Raylib.ClearBackground(Color.BLUE);
+
   Raylib.DrawRectangleRec(playerRect, Color.LIME);
-  
-  Raylib.DrawTexture(playerImage, 45, 70, Color.WHITE);
+
+  Raylib.DrawTexture(playerImage, (int)playerRect.x, (int)playerRect.y, Color.WHITE);
 
   Raylib.EndDrawing();
 }
