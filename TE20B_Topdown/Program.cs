10ded76456c0df89f5f2620eda7356bbba8ffd52
Mikael Bergström <krank23@gmail.com>
2021-12-01 10:16:28 +0100

    CheckCollision

--- TE20B_Topdown/Program.cs
+++ TE20B_Topdown/Program.cs
@@ -9,16 +9,37 @@ float speed = 2.3f;
 Texture2D playerImage = Raylib.LoadTexture("happy.png");
 Rectangle playerRect = new Rectangle(100, 70, playerImage.width, playerImage.height);
 
+Rectangle r1 = new Rectangle(200, 200, 50, 50);
+Rectangle r2 = new Rectangle(125, 225, 50, 50);
+
+Color r2Color = Color.DARKBROWN;
+
 while (!Raylib.WindowShouldClose())
 {
   if (Raylib.IsKeyDown(KeyboardKey.KEY_W)) playerRect.y -= speed;
   if (Raylib.IsKeyDown(KeyboardKey.KEY_S)) playerRect.y += speed;
   if (Raylib.IsKeyDown(KeyboardKey.KEY_A)) playerRect.x -= speed;
   if (Raylib.IsKeyDown(KeyboardKey.KEY_D)) playerRect.x += speed;
+
+  if (Raylib.CheckCollisionRecs(r1, r2))
+  {
+    r2Color = Color.BEIGE;
+  }
+  else
+  {
+    r2Color = Color.RED;
+  }
+
+  r2.x += 0.25f;
+
   
   Raylib.BeginDrawing();
 
   Raylib.ClearBackground(Color.BLUE);
+
+  Raylib.DrawRectangleRec(r1, Color.GREEN);
+  Raylib.DrawRectangleRec(r2, r2Color);
+
 
   Raylib.DrawRectangleRec(playerRect, Color.LIME);
 
