5407fc535e77619cd3c5645222abeb7744f27440
Mikael Bergström <krank23@gmail.com>
2021-12-07 10:45:29 +0100

    DrawText

--- TE20B_Topdown/Program.cs
+++ TE20B_Topdown/Program.cs
@@ -1,14 +1,17 @@
 ﻿using System;
+using System.Numerics;
 using Raylib_cs;
 
 Raylib.InitWindow(800, 600, "2D-topdown game");
 Raylib.SetTargetFPS(60);
 
 float speed = 2.3f;
+Vector2 textPos = new Vector2(50, 25);
 
 Texture2D playerImage = Raylib.LoadTexture("happy.png");
 Rectangle playerRect = new Rectangle(100, 70, playerImage.width, playerImage.height);
 
+Font comic = Raylib.LoadFont("ComicNeue.ttf");
 Rectangle doorRect = new Rectangle(200, 200, 32, 32);
 
 string level = "start";
@@ -17,10 +20,7 @@ while (!Raylib.WindowShouldClose())
 {
   if (level == "start" || level == "corridor")
   {
-    if (Raylib.IsKeyDown(KeyboardKey.KEY_W)) playerRect.y -= speed;
-    if (Raylib.IsKeyDown(KeyboardKey.KEY_S)) playerRect.y += speed;
-    if (Raylib.IsKeyDown(KeyboardKey.KEY_A)) playerRect.x -= speed;
-    if (Raylib.IsKeyDown(KeyboardKey.KEY_D)) playerRect.x += speed;
+    playerRect = ReadMovement(playerRect, speed);
   }
 
   if (level == "start")
@@ -37,7 +37,7 @@ while (!Raylib.WindowShouldClose())
   }
   else if (level == "corridor")
   {
-    if (playerRect.x <0)
+    if (playerRect.x < 0)
     {
       level = "start";
       playerRect.x = 800 - playerRect.width;
@@ -45,6 +45,9 @@ while (!Raylib.WindowShouldClose())
   }
 
   Raylib.BeginDrawing();
+
+  // Raylib.DrawText(playerRect.x.ToString(), 100, 100, 40, Color.DARKBLUE);
+  Raylib.DrawTextEx(comic, "HEJ", textPos, 80, 0, Color.BEIGE);
 
   if (level == "start")
   {
@@ -67,4 +70,15 @@ while (!Raylib.WindowShouldClose())
 
 
   Raylib.EndDrawing();
+}
+
+
+static Rectangle ReadMovement(Rectangle playerRect, float speed)
+{
+  if (Raylib.IsKeyDown(KeyboardKey.KEY_W)) playerRect.y -= speed;
+  if (Raylib.IsKeyDown(KeyboardKey.KEY_S)) playerRect.y += speed;
+  if (Raylib.IsKeyDown(KeyboardKey.KEY_A)) playerRect.x -= speed;
+  if (Raylib.IsKeyDown(KeyboardKey.KEY_D)) playerRect.x += speed;
+
+  return playerRect;
 }
